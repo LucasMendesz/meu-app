d7b9562eb3ed1359abdea58ceed09d807befb58f
Lucas Mendes <lucasaraujo12345@outlook.com.br>
2022-10-10 11:51:36 -0300

    Objeto de estilos CSS

--- src/Components/layout/Cabecalho.jsx
+++ src/Components/layout/Cabecalho.jsx
@@ -1,11 +1,17 @@
 import Menu from "../Menu";
+
+const estilos = {
+  color: "white",
+  backgroundColor: "Black",
+};
+
 const Cabecalho = () => {
   return (
     /* CSS IN JS */
 
     /* CSS inline (usa propriedade style e sintaxe de objeto) */
     <header style={{ backgroundColor: "purple", textAlign: "center" }}>
-      <h1>Interface React</h1>
+      <h1 style={estilos}>Interface React</h1>
       <Menu />
     </header>
   );
