d46b7c88ad7757080b7548fb0b00ba898b0fb013
Lucas Mendes <lucasaraujo12345@outlook.com.br>
2022-10-11 11:27:32 -0300

    Style Components
    Instalação e uso

--- src/Components/layout/Cabecalho.jsx
+++ src/Components/layout/Cabecalho.jsx
@@ -1,10 +1,37 @@
+import styled from "styled-components";
 import Menu from "../Menu";
+
+const periodo = "Diurno";
+
+/* Tagged de Template para uso com styled components */
+const StyledCabecalho = styled.header`
+  background-color: ${periodo === "Diurno" ? "lightblue" : "darkblue"};
+  text-align: center;
+
+  h1 {
+    color: #262626;
+  }
+
+  p {
+    font-weight: bold;
+    color: purple;
+    font-size: 2rem;
+
+    &:hover {
+      background-color: violet;
+      color: #f7f7f7;
+      cursor: pointer;
+    }
+  }
+`;
+
 const Cabecalho = () => {
   return (
-    <header>
+    <StyledCabecalho>
       <h1>Interface React</h1>
       <Menu />
-    </header>
+      <p>Teste</p>
+    </StyledCabecalho>
   );
 };
 export default Cabecalho;
