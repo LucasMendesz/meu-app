9666b6b1f13e12ec5d9dd56c6e6ae369d30be5dc
Lucas Mendes <lucasaraujo12345@outlook.com.br>
2022-10-13 11:49:37 -0300

    Mais exemplo de useState

--- src/Components/layout/Rodape.jsx
+++ src/Components/layout/Rodape.jsx
@@ -1,8 +1,29 @@
+import { useState } from "react";
+
 const Rodape = () => {
+  const [estilos, setEstilos] = useState({
+    textAlign: "center",
+    textTransform: "uppercase",
+    border: "solid 2px red",
+  });
+
+  const esconder = () => setEstilos({ display: "none" });
+  const exibir = () => setEstilos({ display: "block" });
+
   return (
-    <footer>
-      <h2>Rodapé!</h2>
-    </footer>
+    <>
+      <button>
+        <span onMouseOver={esconder}>Esconder</span>
+      </button>
+
+      <button>
+        <span onMouseOver={exibir}>Exibir</span>
+      </button>
+
+      <footer style={estilos}>
+        <h2>Rodapé!</h2>
+      </footer>
+    </>
   );
 };
 export default Rodape;
