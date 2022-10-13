036f32081368cb69be79c5813833fb724a5c67a9
Lucas Mendes <lucasaraujo12345@outlook.com.br>
2022-10-13 10:41:05 -0300

    Tipos de exemplos no react

--- src/Components/layout/Cabecalho.jsx
+++ src/Components/layout/Cabecalho.jsx
@@ -1,7 +1,11 @@
 import Menu from "../Menu";
 const Cabecalho = () => {
+  const exemplo2 = () => {};
   return (
     <header>
+      <button onClick={() => console.log("exemplo 1")}>Exemplo 1</button>
+      <button onClick={exemplo2}>Exemplo 2</button>
+      <hr />
       <h1>Interface React</h1>
       <Menu />
     </header>
